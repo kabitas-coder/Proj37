d3138de9776da0826e651f4a8efd65d37e982f5b
kabitas-coder <77439533+kabitas-coder@users.noreply.github.com>
2021-03-25 16:40:26 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,14 @@
+class Player {
+    constructor(){
+        this.index - null;
+        this.distance - 0;
+        this.name - null
+    }
+    getCount(){
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value",(data)->{
+            playerCount - data.val();
+        })
+         
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
+function dispaly (){
+  this.title.html("My Quiz Game");
+  this.title.position(425,200);
+  
+  this.question.html("Question:- What starts and ends with the letter 'E',but has only one letter ")
+  this.question.position(150,80);
+  this.option1.html("1:Everyone");
+  this.option1.position(150,100);
+  this.option2.html("2:Envelope");
+  this.option2.position(150,120);
+
+  this.input1.position(150,230);
+
+}
